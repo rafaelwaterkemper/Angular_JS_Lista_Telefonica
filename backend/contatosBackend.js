a36a7589227258c80b18816276f55d0d481fe6c6
Rafael Marangoni Waterkemper <rafaelwaterkemper@gmail.com>
2017-07-25 22:21:07 -0300

    Alterado para versão 1.6 do angular

--- backend/contatosBackend.js
+++ backend/contatosBackend.js
@@ -7,37 +7,37 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 var contatos = [
-	{nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
-	{nome: "Sandra", telefone: "9999-3333", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
-	{nome: "Mariana", telefone: "9999-9999", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
+  { nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: { nome: "Oi", codigo: 14, categoria: "Celular" } },
+  { nome: "Sandra", telefone: "9999-3333", data: new Date(), operadora: { nome: "Vivo", codigo: 15, categoria: "Celular" } },
+  { nome: "Mariana", telefone: "9999-9999", data: new Date(), operadora: { nome: "Tim", codigo: 41, categoria: "Celular" } }
 ];
 var operadoras = [
-	{nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
-	{nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
-	{nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
-	{nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1},
-	{nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2}
+  { nome: "Oi", codigo: 14, categoria: "Celular", preco: 2 },
+  { nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1 },
+  { nome: "Tim", codigo: 41, categoria: "Celular", preco: 3 },
+  { nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1 },
+  { nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2 }
 ];
 
 app.listen(process.env.PORT || 3412);
 console.log('Startando na porta ' + process.env.PORT);
 
-app.all('*', function(req, res, next) {
+app.all('*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-app.get('/contatos', function(req, res) {
+app.get('/contatos', function (req, res) {
   res.json(contatos);
 });
 
-app.post('/contatos', function(req, res) {
+app.post('/contatos', function (req, res) {
   contatos.push(req.body);
   res.json(req.body);
 });
 
-app.get('/operadoras', function(req, res) {
+app.get('/operadoras', function (req, res) {
   res.json(operadoras);
 });
